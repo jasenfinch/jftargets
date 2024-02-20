0b85b253435d8fb1be9bbcd191b9f91ac2788fc3
jasenfinch <jsf9@aber.ac.uk>
2024-02-20 15:52:09 +0000

    tidy tests folder

--- tests/testthat/_targets.R
+++
@@ -1,4 +0,0 @@
-library(targets)
-list(targets::tar_target(iris_plot, ggplot2::ggplot(iris, ggplot2::aes(x = Sepal.Length, 
-    y = Sepal.Width)) + ggplot2::geom_point()), jftargets::tar_png(iris_plot, 
-    export_path = export_path))
