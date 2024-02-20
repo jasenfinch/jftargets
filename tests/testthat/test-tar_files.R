cda41c4d2dc8d4536ac063ffdd13c1d647c17e5a
jasenfinch <jsf9@aber.ac.uk>
2024-02-20 15:50:43 +0000

    improve tar_files test

--- tests/testthat/test-tar_files.R
+++ tests/testthat/test-tar_files.R
@@ -1,6 +1,21 @@
 
 test_that("tar_files works", {
-  targets <- tar_files(test,paste0(tempdir(),'test.csv'))
   
-  expect_identical(class(targets),'list')
+  file_path <- tempdir()
+  
+  targets::tar_dir({
+    targets::tar_script({
+      list(
+        targets::tar_target(
+          iris,
+          write.csv(iris,paste0(!!file_path,'/iris.csv'))
+        ),
+        jftargets::tar_files(test,paste0(!!file_path,'/iris.csv'))
+      )
+    })
+    
+    targets::tar_make(callr_function = NULL)
+  })
+  
+  expect_true(file.exists(paste0(!!file_path,'/iris.csv')))
 })
